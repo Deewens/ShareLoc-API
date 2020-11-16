61ed03dcd19c5ed2a6a03874edc0fce07930bdda
Adrien Dudon <adrien.dudon@etu.unistra.fr>
2020-11-16 19:21:26 +0100

    Revert "Revert "Fix JPA/JTA errors""
    This reverts commit 2280ffd0cbbc11e57758c2c0f70319f57e6afd8f

--- src/main/java/shareloc/model/dao/UserDAO.java
+++ src/main/java/shareloc/model/dao/UserDAO.java
@@ -1,9 +1,10 @@
-package model.dao;
+package shareloc.model.dao;
 
-import model.ejb.User;
+import shareloc.model.ejb.User;
 
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public class UserDAO extends DAO<User> {
@@ -11,11 +12,27 @@ public class UserDAO extends DAO<User> {
         super(User.class);
     }
 
+    @Transactional
     public Optional<User> findByEmail(String email) {
         User user;
 
         Query query = em.createQuery("SELECT u FROM User u WHERE u.email = :email");
         query.setParameter("email", email);
+
+        try {
+            user = (User) query.getSingleResult();
+            return Optional.of(user);
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Transactional
+    public Optional<User> findByPseudo(String pseudo) {
+        User user;
+
+        Query query = em.createQuery("SELECT u FROM User u WHERE u.pseudo = :pseudo");
+        query.setParameter("pseudo", pseudo);
 
         try {
             user = (User) query.getSingleResult();
