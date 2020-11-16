8bfc12493544cd00eeace8d4108983326298240d
Deewens <deeewens@gmail.com>
2020-11-16 19:04:04 +0100

    Fix JPA/JTA errors

--- src/main/java/shareloc/model/dao/DAO.java
+++ src/main/java/shareloc/model/dao/DAO.java
@@ -1,44 +1,34 @@
-package model.dao;
+package shareloc.model.dao;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 public abstract class DAO<T> {
-
-    @PersistenceUnit(unitName = "MariaDB")
-    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("MariaDB");
     @PersistenceContext(unitName = "MariaDB")
     protected EntityManager em;
     private Class<T> entityClass;
 
     public DAO(Class<T> entityClass) {
         this.entityClass = entityClass;
-        this.em = getEntityManager();
     }
 
-    private EntityManager getEntityManager() {
-        if (em == null)
-            em = emf.createEntityManager();
-
-        return em;
-    }
-
+    @Transactional
     public T create(T entity) {
-        em.getTransaction().begin();
         em.persist(entity);
-        em.getTransaction().commit();
 
         return entity;
     }
 
+    @Transactional
     public void update(T entity) {
-        em.getTransaction().begin();
         em.merge(entity);
-        em.getTransaction().commit();
     }
 
     public void delete(T entity) {
